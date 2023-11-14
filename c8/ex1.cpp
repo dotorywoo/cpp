627d4c45dba3813a383108c43114f36b79745b78
dotorywoo <150768793+dotorywoo@users.noreply.github.com>
2023-11-14 17:51:06 +0900

    Update ex1.cpp

--- c8/ex1.cpp
+++ c8/ex1.cpp
@@ -2,43 +2,33 @@
 using namespace std;
 class fruit {
 public:
-	// constructor
-	fruit() {
-		price = 200; many = 2; day = 12;  // default values
-	}
-	fruit(float p, int n) {
-		price = p;
-		many = n;
-		day = 12;
-	}
-	fruit(float p, int n, int d) {
-		price = p;
-		many = n;
-		day = d;
-		
-	}
-	// member data
-	int many;
-	float price;
-	int day;
-	// member function
-	float show() {
-		return price * many;
-	}
-	void show_all() {
-		cout << price << " " << many << " " << day;
-	}
-};
+   fruit() {
+      price = 200;
+      number = 2;
+      day = 12;
+   }
+   fruit(int p, int n, int d) {
+      price = p;
+      number = n;
+      day = d;
+   }
+   float price;
+   int number;
+   int day;
 
+   float show() {
+      return price * number;
+   }
+   void show_all() {
+      cout << price << endl;
+      cout << number << endl;
+      cout << day << endl;
+   }
+};
 int main() {
-	fruit apple, pear, peach,mango;
-	pear = fruit(300, 5);
-	peach = fruit(1000, 5);
-	mango = fruit(700, 8);
-	cout << apple.show() << endl;
-	cout << pear.show() << endl;
-	cout << peach.show() << endl;
-	cout << " peach " << peach.price << endl;
-	mango.show_all();
-	return 123;
+   fruit mango, apple, pear, peach;
+   mango = fruit(700, 8, 12);
+   mango.show_all();
+   cout << mango.show() << endl;
+   return 666;
 }
