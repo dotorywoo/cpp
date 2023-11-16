7eb57d70ec2c2452503253d7b3c05236d38595da
dotorywoo <150768793+dotorywoo@users.noreply.github.com>
2023-11-17 03:59:29 +0900

    Create wk11.cpp

---
+++ lol/m2/wk11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include "LoLCha.h"
+using namespace std;
+void main() {
+    LoL alkali(5, 10, 2, 3, -5, 0.1, 10);
+    alkali.f3("alkali.dat");
+
+    LoL amumu(7, 100, -3.5, 4, 100, 10, 15);
+    amumu.f3("amumu.dat");
+
+    LoL annie(12, 17, 0.01, 0.2, 1.5, 0.08, 20);
+    annie.f3("annie.dat");
+
+    LoL ashe(10, 20, 4.2, -5.3, 2.1, 0.2, 5);
+    ashe.f3("ashe.dat");
+
+
+
+
+}
