f2bc503d769dc899df38828604ca619f9dfd21c3
dotorywoo <150768793+dotorywoo@users.noreply.github.com>
2023-11-17 03:56:26 +0900

    Update lolcha.cpp

--- lol/m2/lolcha.cpp
+++ lol/m2/lolcha.cpp
@@ -1,1 +1,20 @@
+#pragma once
+#include <fstream>
+using namespace std;
+class LoL {
+public:
+	LoL() {};
+	LoL(int N1, int N2, float a, float b, float x0, float dx, int m);
+	int* f1Result;
+	float* f2Result;
+	int N1, N2, m;
+	float a, b, x0, dx;
+	ofstream dd;
+	void f0(string);
+	int* f1();
+	float* f2();
+	void f3(string filename);
+	~LoL();
+
+};
 
